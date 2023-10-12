856d0fed5d3ec1455035aa13df440167ca4a55d8
makka999 <mateuszk-98@wp.pl>
2023-10-12 17:29:12 +0200

    Test kontrolera

---
+++ CB_proj/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using CB_proj.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CB_proj.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private  CB_projContext _context;
+
+
+        // GET: api/<AccountController>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var accounts = await _context.Accounts.ToListAsync();
+
+            return (IActionResult)accounts;
+        }
+
+        // GET api/<AccountController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+
+
+
+            return "value";
+        }
+
+        // POST api/<AccountController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<AccountController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<AccountController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
